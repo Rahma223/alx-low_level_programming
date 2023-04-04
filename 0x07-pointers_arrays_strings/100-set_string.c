e6f9857a10502e5610a01d4bb21491715c189464
Rahma223 <01125617669r@gmail.com>
2023-04-04 15:15:57 -0700

    a

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * set_string - a function that sets the value of a pointer to a char.
+ * @s: pointer to pointer
+ * @to: pointer char
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums - a function that prints the sum of the two diagonals
+ * @a: 2d Array
+ * @size: size of array
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, s = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", s);
+	printf("%d\n", s2);
+}
