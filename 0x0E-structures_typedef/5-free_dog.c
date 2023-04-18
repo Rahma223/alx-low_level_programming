e03e44b5ea6a77b0664192c8e736ebed1116ef66
Rahma223 <01125617669r@gmail.com>
2023-04-18 07:52:43 -0700

    A

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs.
+ * @d: yo dawg
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
