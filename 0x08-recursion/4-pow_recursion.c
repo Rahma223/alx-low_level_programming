c250eaa5c7267e735dcac7e56c9bf3baf7d62003
Rahma <01125617669r@gmail.com>
2023-04-28 13:41:21 -0700

    a

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,24 +1,17 @@
 #include "main.h"
 
 /**
- * _pow_recursion - a function that returns the value of x power y
- * @x: number we get the power
- * @y: power of number
- * Return: power of x
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
+ *
+ * Return: result of the power
  */
-
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
-	else if (y == 1)
-	{
-		return (x);
-	}
-	else
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,36 +1,32 @@
 #include "main.h"
-#include <stdio.h>
+
+int actual_prime(int n, int i);
 
 /**
- * is_prime_number - a function that check thr number is prime
- * @n: number we checked
- * Return: 0 or 1
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 if not
  */
-
 int is_prime_number(int n)
 {
-	return (check_prime(n, 2));
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
 }
 
 /**
- * check_prime - a function that check thr number is prime
- * @n: the number we checked
- * @i: other number
- * Return: 1 if number prime ,0 if not
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
  */
-
-int check_prime(int n, int i)
+int actual_prime(int n, int i)
 {
-	if (i >= n && n > 1)
-	{
+	if (i == 1)
 		return (1);
-	}
-	else if (n % i == 0 || n <= 1)
-	{
-	return (0);
-	}
-	else
-	{
-	return (check_prime(n, i + 1));
-	}
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
 }
