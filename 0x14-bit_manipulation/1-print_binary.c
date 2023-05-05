69eee31f19d1c66f7e87a848f14bfd543b18711a
Rahma <01125617669r@gmail.com>
2023-05-04 17:46:55 -0700

    a

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -21,5 +21,5 @@ void print_binary(unsigned long int n)
 			_putchar('0');
 	}
 	if (!pri)
-		_pitchar('0');
+		_putchar('0');
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,17 +1,24 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * set_bit -  a function that sets the value of a bit to 1 at a given index.
- * @n: the number to index
- * @index: the bit to set
- * Return: 1 if successful, -1 if error
+ * clear_bit - set the value of a bit to 0 at a given index
+ * @n: pointer to decimal number to change
+ * @index: index position to change
+ * Return: 1 if it worked, -1 if error
  */
-
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
+	unsigned long int i;
+	unsigned int hold;
+
+	if (index > 64)
 		return (-1);
-	if (*n & 1L << index)
-		*n ^= 1L << index;
+	hold = index;
+	for (i = 1; hold > 0; i *= 2, hold--)
+		;
+
+	if ((*n >> index) & 1)
+		*n -= i;
+
 	return (1);
 }
